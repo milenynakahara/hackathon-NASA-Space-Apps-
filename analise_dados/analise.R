9533ec29a8c4fa5b8d9429171d4c05fa6679c5f9
Valéria Nicéria <valerianiceria@gmail.com>
2020-05-31 09:04:50 -0300

    Adicionado continente

--- analise_dados/analise.R
+++ analise_dados/analise.R
@@ -4,7 +4,7 @@ library(zoo)
 source("funcoes.R", encoding = "UTF-8")
 
 
-# Carregando os dados
+# Carregando os dados sobre o COVID
 dados_covid <- readRDS("dados/dados_covid_mundo.rds")
 
 
@@ -26,5 +26,22 @@ dados_novos_casos <- dados_covid %>%
 # Casos acumulados
 dados_casos_acumulados <- dados_covid %>% 
   filter(date == ultimo_dia)
+
+
+
+# Carregando os dados - para fazer o depara - países e continentes
+df_paises_continentes <- read.csv2("dados/datasets_14947_19943_countryContinent.csv", sep = ",") %>% 
+  dplyr::select(country, continent, sub_region)  %>% 
+  as.data.frame() %>% 
+  mutate(country = atualizar_nome_paises(as.character(country)))
+
+
+# Juntando os dados dos países com os continentes - pelo nome do país
+df_final_casos_novos <- dados_novos_casos %>% 
+  left_join(df_paises_continentes, by=c("Country.Region"="country"))
+
+
+
+
 
 
